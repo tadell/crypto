f067a4e8bca562804cdffc76c4fe746140c50cb8
tt adel <ttadel.1987@gmail.com>
2021-06-17 23:43:56 +0430

    use androidx and fragment list layout design

--- app/src/main/java/com/example/crypto/MainActivity.kt
+++ app/src/main/java/com/example/crypto/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.crypto
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
